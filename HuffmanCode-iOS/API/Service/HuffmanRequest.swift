2971c9466296f7ecf512860125ccff8cb856e430
yochidros <mm9.movement.trb@gmail.com>
2017-10-14 17:06:37 +0900

    sameone

--- HuffmanCode-iOS/API/Service/HuffmanRequest.swift
+++ HuffmanCode-iOS/API/Service/HuffmanRequest.swift
@@ -7,3 +7,35 @@
 //
 
 import Foundation
+import APIKit
+
+protocol HuffmanRequest: Request {
+}
+
+extension HuffmanRequest {
+    var baseURL: URL {
+        return URL(string: "http://localhost:8000")!
+    }
+}
+
+extension HuffmanRequest where Response: Decodable {
+    var dataParser: DataParser {
+        return DecodableDataParser()
+    }
+    func response(from object: Any, urlResponse: HTTPURLResponse) throws -> Response {
+        guard let data = object as? Data else {
+            throw ResponseError.unexpectedObject(object)
+        }
+        return try JSONDecoder().decode(Response.self, from: data)
+    }
+}
+
+
+class DecodableDataParser: DataParser {
+    var contentType: String? {
+        return "application/json"
+    }
+    func parse(data: Data) throws -> Any {
+        return data
+    }
+}
